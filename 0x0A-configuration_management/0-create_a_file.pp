80f9c43689aa8506029282db194e3fab381d1035
beboo22 <mt557829@gmail.com>
2023-09-22 23:00:39 +0300

    task

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# Creates a file
-file { '/tmp/school':
+# This Puppet manifest creates a file in /tmp
+
+file { 'holberton':
   ensure  => file,
-  path    => '/tmp/school',
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
