dd65f24aa828c40846b34f53256b61247ff8a707
root <root@Ziad-Nasser>
2024-08-13 19:38:24 +0300

    welcome back

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""task zero."""
+import requests
+
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers on subreddit."""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    result = requests.get(url, headers=headers, allow_redirects=False)
+    if result.status_code == 404:
+        return 0
+    result = result.json().get("data")
+    return result.get("subscribers")
