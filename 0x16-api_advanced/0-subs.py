c43c72cbcb6ac813097b6ead56049238989a6ad3
root <root@Ziad-Nasser>
2024-08-13 19:47:05 +0300

    welcome back

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""task zero."""
+"""first task."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers on subreddit."""
+    """Return the total number of subscribers on a given subreddit."""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    result = requests.get(url, headers=headers, allow_redirects=False)
-    if result.status_code == 404:
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code == 404:
         return 0
-    result = result.json().get("data")
+    result = res.json().get("data")
     return result.get("subscribers")
