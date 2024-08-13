6291a1043c06329801404122031c455ad9c19b1a
Ziad-Nasser <ziadnasser177@gmail.com>
2024-08-13 20:51:54 +0300

    welcome back

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,19 +4,19 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Get Subscriber from reddit API"""
+    """Get number of subscribers from reddit API"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    headers = {"User-Agent": "MyApp/1.0"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
+    }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
-
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    elif response.status_code == 302:
+        return data["data"]["subscribers"]
+    elif response.status_code in [301, 302, 404]:
         return 0
     else:
         return 0
