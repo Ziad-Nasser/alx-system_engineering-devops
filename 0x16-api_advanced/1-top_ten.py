74e0f91c896fcfc4e4397a72c1ddeb5c964384c3
Ziad-Nasser <ziadnasser177@gmail.com>
2024-08-13 20:50:08 +0300

    welcome back

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,16 +4,21 @@ import requests
 
 
 def top_ten(subreddit):
-    """Get Top ten from reddit API"""
-    url = f"https://www.reddit.com/r/{subreddit}/top.json?limit=10"
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    headers = {"User-Agent": "MyApp/1.0"}
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/your_username)"
+    }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code == 200:
+    if response.status_code == 404:
+        print("None")
+        return
+    elif response.status_code == 200:
         data = response.json()
         for post in data["data"]["children"]:
             print(post["data"]["title"])
     else:
-        print(None)
+        print("None")
